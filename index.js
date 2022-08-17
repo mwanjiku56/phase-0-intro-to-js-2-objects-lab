aee3c6f8adbd34ad8e8ca51cb67e2fb9f7a5391c
mercy wanjiku maruga <mercy.maruga@student.moringaschool.com>
2022-08-17 23:42:13 +0300

    Complted assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee={
+    name:"Mercy",
+    streetAdress:"4th street kasarani",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     const newEmployee = { ...employee };
+
+     newEmployee[key] = value;
+  
+    return newEmployee;
+  }
+  let destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => { employee[key] = value; return employee; }
+
+function deleteFromEmployeeByKey(employee, key){
+  const updateEmployee = {...employee}
+    delete updateEmployee[key];
+    return updateEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  const employeeByKey = employee;
+  delete employeeByKey[key];
+  return employeeByKey;
+}
+
